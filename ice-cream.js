2dc7940527d1d4b9d977d5a1e25a0fb04d777ae9
Xihai Luo <xihai.luo01@gmail.com>
2021-09-20 15:33:02 +0000

    added ice-cream and packing list exercises

---
+++ ice-cream.js
@@ -0,0 +1,15 @@
+const iceCreamFlavours = ['chocolate', 'vanilla', 'cookies and cream', 'rocky road', 'strawberry'];
+console.log(iceCreamFlavours);
+
+//adding a flavour - root beer
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+
+//get the first flavour
+console.log(iceCreamFlavours[0]);
+
+//get the last flavour
+console.log(iceCreamFlavours[5]);
+
+//get the total number of flavours
+console.log(iceCreamFlavours.length);
